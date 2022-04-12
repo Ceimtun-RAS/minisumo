858f5fcf5171309eef511bf6bc15403237fda8fb
Andrés Morales <mora200217@hotmail.com>
2022-04-12 14:34:49 -0500

    Base Sensor class

---
+++ Sensor.ino
@@ -0,0 +1,27 @@
+/*
+* Sensor 
+*
+* Clase para caracterización y encapsulamiento de los 
+* sensores de linea y distancia. 
+*
+* Author: Linda Méndez 
+* date: 12-April-2022 v0.1
+* Project: Minisumo
+*
+*/ 
+
+class Sensor{
+    public:
+        Sensor(); 
+        int displayName(); 
+
+    private: 
+}
+
+Sensor::Sensor(){
+    print("Me creaste! uwu"); 
+}
+Sensor::int displayName(){
+
+}
+
